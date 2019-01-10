870031c50d698fd4933c36467d437b97279faef6
R-Cole <randy@goodglue.com>
2019-01-10 14:11:35 -0700

    heroku 2

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 export const API_BASE_URL = 
-process.env.REACT_APP_API_BASE_URL || 'https://goodcall-server.herokuapp.com';
+process.env.REACT_APP_API_BASE_URL || 'https://goodcall-server.herokuapp.com/api';
+
 
 // module.exports = {
 //   CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:3000'
